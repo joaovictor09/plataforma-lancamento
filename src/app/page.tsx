1c47457d82ea147ca2d4777300648d8ad88fbd7c
João Victor da Silva <joaovictordasilva0911@gmail.com>
2023-06-07 00:24:04 -0300

    Finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Image from 'next/image'
 import Logo from '../assets/Logo.svg'
 import BackgroundImage from '../assets/bg-palestra.png'
@@ -9,7 +7,6 @@ import {
   Building2,
   Calendar,
   Clock8,
-  Loader,
   Rocket,
   RotateCw,
   Smartphone,
@@ -17,45 +14,9 @@ import {
   Trophy,
   Users2,
 } from 'lucide-react'
-import { useRouter } from 'next/navigation'
-import { useState, FormEvent } from 'react'
-import { client } from '@/lib/apollo'
-import { gql } from '@apollo/client'
-
-async function fetchCreateSubscriberMutation(name: string, email: string) {
-  await client.query({
-    query: gql`
-      mutation CreateSubscriber($name: String!, $email: String!) {
-        upsertSubscriber(
-          where: { email: $email }
-          upsert: { create: { name: $name, email: $email }, update: {} }
-        ) {
-          name
-          email
-        }
-      }
-    `,
-    variables: { name, email },
-    fetchPolicy: 'no-cache',
-  })
-}
+import { CreateSubscriberForm } from './components/CreateSubscriberForm'
 
 export default function Home() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [loading, setLoading] = useState(false)
-  const router = useRouter()
-
-  async function handleSubscribe(event: FormEvent) {
-    event.preventDefault()
-    setLoading(true)
-
-    await fetchCreateSubscriberMutation(name, email)
-
-    router.push('/thank-you')
-    setLoading(false)
-  }
-
   return (
     <main className="flex min-h-screen w-full flex-1 flex-col items-center justify-center bg-right-top bg-no-repeat lg:bg-hero">
       {/* Hero and form */}
@@ -99,35 +60,7 @@ export default function Home() {
             </div>
           </div>
 
-          <form
-            className="mt-5 flex w-full max-w-md flex-col gap-2"
-            onSubmit={handleSubscribe}
-          >
-            <input
-              className="h-14 rounded border border-zinc-600 bg-zinc-200 px-5 outline-none ring-0 placeholder:text-zinc-700 focus:border-blue-900 focus:ring-1 focus:ring-blue-900"
-              type="text"
-              placeholder="Seu nome completo"
-              required
-              minLength={3}
-              onChange={(event) => setName(event.target.value)}
-            />
-            <input
-              className="h-14 rounded border border-zinc-600 bg-zinc-200 px-5 outline-none ring-0 placeholder:text-zinc-700 focus:border-blue-900 focus:ring-1 focus:ring-blue-900"
-              type="email"
-              required
-              placeholder="Digite seu email"
-              onChange={(event) => setEmail(event.target.value)}
-            />
-
-            <button
-              type="submit"
-              className="mt-4 flex items-center justify-center gap-4 rounded bg-blue-900 py-4 text-sm font-bold uppercase text-white transition-colors hover:bg-blue-950 disabled:cursor-not-allowed disabled:opacity-50"
-              disabled={loading}
-            >
-              {loading && <Loader className="animate-spin" />}
-              <span>Garantir minha vaga</span>
-            </button>
-          </form>
+          <CreateSubscriberForm />
         </div>
       </div>
 
