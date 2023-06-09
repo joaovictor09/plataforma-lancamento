6a5cba6ddf4ef082167c53ff6e7d5cd387861ef9
João Victor da Silva <joaovictordasilva0911@gmail.com>
2023-06-09 08:32:26 -0300

    Fix: Embed domain name

--- src/app/event/lesson/[slug]/components/Chat.tsx
+++ src/app/event/lesson/[slug]/components/Chat.tsx
@@ -3,6 +3,12 @@
 import Iframe from 'react-iframe'
 
 export function Chat({ liveId }: { liveId: string }) {
+  const hostname =
+    typeof window !== 'undefined' && window.location.hostname
+      ? window.location.hostname
+      : ''
+  console.log(hostname)
+
   return (
     <Iframe
       url={`https://www.youtube.com/live_chat?v=${liveId}&embed_domain=localhost`}
