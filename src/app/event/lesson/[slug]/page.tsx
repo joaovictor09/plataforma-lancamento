ac4a02be26e548ca1b63cc566208998f39064371
João Victor da Silva <joaovictordasilva0911@gmail.com>
2023-06-10 14:08:35 -0300

    Added generateStaticParams

--- src/app/event/lesson/[slug]/page.tsx
+++ src/app/event/lesson/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { client } from '@/lib/apollo'
+import { gql } from '@apollo/client'
 import { Sidebar } from '../../../components/Sidebar'
 import { Video } from '../../../components/Video'
 
@@ -7,28 +9,28 @@ interface paramsProps {
 
 export const dynamicParams = true
 
-// async function fetchAllLessonsSlug() {
-//   const { data } = await client.query({
-//     query: gql`
-//       query getAllLessonsQuery {
-//         lessons {
-//           slug
-//         }
-//       }
-//     `,
-//     fetchPolicy: 'no-cache',
-//   })
+async function fetchAllLessonsSlug() {
+  const { data } = await client.query({
+    query: gql`
+      query getAllLessonsQuery {
+        lessons {
+          slug
+        }
+      }
+    `,
+    fetchPolicy: 'no-cache',
+  })
 
-//   return data
-// }
+  return data
+}
 
-// export async function generateStaticParams() {
-//   const { lessons } = await fetchAllLessonsSlug()
+export async function generateStaticParams() {
+  const { lessons } = await fetchAllLessonsSlug()
 
-//   return lessons.map((lesson) => ({
-//     slug: lesson.slug,
-//   }))
-// }
+  return lessons.map((lesson) => ({
+    slug: lesson.slug,
+  }))
+}
 
 export default async function Event({ params }: { params: { slug: string } }) {
   // if !slug - query buscar a primeira aula - redirecionar para o slug da primeira aula
